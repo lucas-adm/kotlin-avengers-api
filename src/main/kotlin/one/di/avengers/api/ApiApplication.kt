6b7d1ca75cbd7377c17634a7440af5aeb3d0718d
Lucas Aguiar <vintesetelucas@gmail.com>
2024-03-02 08:23:51 -0300

    init commit

---
+++ src/main/kotlin/one/di/avengers/api/ApiApplication.kt
@@ -0,0 +1,11 @@
+package one.di.avengers.api
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<ApiApplication>(*args)
+}
